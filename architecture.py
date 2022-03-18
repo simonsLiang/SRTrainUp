0d8ddaa7498429db086b223641391965930001bb
simonsLiang <63990611+simonsLiang@users.noreply.github.com>
2022-03-18 19:48:38 +0800

    Update architecture.py

--- architecture.py
+++ architecture.py
@@ -1,6 +1,7 @@
 from SwinIR.models.network_swinir import SwinIR as net
 
-def IMDN(scale=4):
-    model = net(upscale=scale, in_chans=3, img_size=64, window_size=8,
+def IMDN(upscale=4):
+    model = net(upscale=upscale, in_chans=3, img_size=64, window_size=8,
                     img_range=1., depths=[6, 6, 6, 6], embed_dim=48, num_heads=[6, 6, 6, 6],
                     mlp_ratio=2, upsampler='pixelshuffledirect', resi_connection='1conv')
+    return model

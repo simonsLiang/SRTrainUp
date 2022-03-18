d14a96685331c6b124230bbc8b215f2f290d3d62
simonsLiang <63990611+simonsLiang@users.noreply.github.com>
2022-03-18 19:07:14 +0800

    Add files via upload

---
+++ architecture.py
@@ -0,0 +1,6 @@
+from SwinRI.models.network_swinir import SwinIR as net
+
+def IMDN(scale=4):
+    model = net(upscale=scale, in_chans=3, img_size=64, window_size=8,
+                    img_range=1., depths=[6, 6, 6, 6], embed_dim=48, num_heads=[6, 6, 6, 6],
+                    mlp_ratio=2, upsampler='pixelshuffledirect', resi_connection='1conv')

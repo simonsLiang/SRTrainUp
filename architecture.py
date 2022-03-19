48b339eca343093b695b3ef900cb8a902a36069c
simonsLiang <63990611+simonsLiang@users.noreply.github.com>
2022-03-19 17:24:23 +0800

    Update architecture.py

--- architecture.py
+++ architecture.py
@@ -1,7 +1,39 @@
-from SwinIR.models.network_swinir import SwinIR as net
+import torch.nn as nn
+from . import block as B
+import torch
 
-def IMDN(upscale=4):
-    model = net(upscale=upscale, in_chans=3, img_size=48, window_size=8,
-                    img_range=1., depths=[6, 6, 6, 6], embed_dim=48, num_heads=[6, 6, 6, 6],
-                    mlp_ratio=2, upsampler='pixelshuffledirect', resi_connection='1conv')
-    return model
+class IMDN(nn.Module):
+    def __init__(self, in_nc=3, nf=64, num_modules=6, out_nc=3, upscale=4):
+        super(IMDN, self).__init__()
+
+        self.fea_conv = B.conv_layer(in_nc, nf, kernel_size=3)
+
+        # IMDBs
+        self.IMDB1 = B.IMDModule(in_channels=nf)
+        self.IMDB2 = B.IMDModule(in_channels=nf)
+        self.IMDB3 = B.IMDModule(in_channels=nf)
+        self.IMDB4 = B.IMDModule(in_channels=nf)
+        self.IMDB5 = B.IMDModule(in_channels=nf)
+        self.IMDB6 = B.IMDModule(in_channels=nf)
+
+        self.c = B.conv_block(nf * num_modules, nf, kernel_size=1, act_type='lrelu')
+
+        self.LR_conv = B.conv_layer(nf, nf, kernel_size=3)
+
+        upsample_block = B.pixelshuffle_block
+        self.upsampler = upsample_block(nf, out_nc, upscale_factor=upscale)
+
+
+    def forward(self, input):
+        out_fea = self.fea_conv(input)
+        out_B1 = self.IMDB1(out_fea)
+        out_B2 = self.IMDB2(out_B1)
+        out_B3 = self.IMDB3(out_B2)
+        out_B4 = self.IMDB4(out_B3)
+        out_B5 = self.IMDB3(out_B4)
+        out_B6 = self.IMDB4(out_B5)
+    
+        out_B = self.c(torch.cat([out_B1,out_B2,out_B3,out_B4,out_B5,out_B6], dim=1))
+        out_lr = self.LR_conv(out_B) + out_fea
+        output = self.upsampler(out_lr)
+        return output
